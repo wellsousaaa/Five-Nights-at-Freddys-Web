0fb3b375f13c4a973a5a0aeaae2d6fb85fcd3909
wellsousaaa <wendell25sousaferreira@gmail.com>
2021-01-29 14:47:57 -0300

    Doors controller fixed

--- src/Game.js
+++ src/Game.js
@@ -12,104 +12,112 @@ let isBlackout = false;
 let { Ambience } = Media.Sounds;
 Ambience.loop = true;
 
+let officeProps = { leftDoor: false, rightDoor: false };
+
 const Game = ({
-    office,
-    isCameraOpen,
-    energy,
-    gameOver,
-    endGame,
-    dispatch,
+  office,
+  isCameraOpen,
+  energy,
+  gameOver,
+  endGame,
+  dispatch,
 }) => {
-    useEffect(() => {
-        Ambience.currentTime = 0;
-        Ambience.play();
-        isBlackout = false;
-    }, []);
+  useEffect(() => {
+    Ambience.currentTime = 0;
+    Ambience.play();
+    isBlackout = false;
+    officeProps = { leftDoor: false, rightDoor: false };
+  }, []);
 
-    useEffect(() => {
-        if (gameOver) Ambience.pause();
-    }, [gameOver]);
+  useEffect(() => {
+    if (gameOver) Ambience.pause();
+  }, [gameOver]);
 
-    useEffect(() => {
-        if (energy <= 0) {
-            isBlackout = true;
-            Ambience.pause();
-        }
-    }, [energy]);
-
-    useEffect(() => {
-        let newTime = 5000;
-        if (office.leftDoor) newTime -= 1100;
-        if (office.rightDoor) newTime -= 1100;
-        if (office.leftLight) newTime -= 500;
-        if (office.rightLight) newTime -= 500;
-        if (isCameraOpen) newTime -= 1100;
-
-        dispatch({ type: "CHANGE_TIME", content: newTime });
-    }, [
-        office.leftDoor,
-        office.rightDoor,
-        office.leftLight,
-        office.rightLight,
-        isCameraOpen,
-    ]);
-
-    const handleJumpscare = (character) => {
-        if (isBlackout || gameOver) return;
-        dispatch({
-            type: "CHANGE_ANIMATRONIC",
-            animatronic: character,
-            animatronicState: {
-                door: null,
-                camera: null,
-                jumpscare: true,
-            },
-        });
-
-        dispatch({ type: "CHANGE_JUMPSCARE", animatronic: character });
-        if (character === "Foxy" || character === "Freddy")
-            dispatch({ type: "FORCE_CAMERA_CLOSE" });
-        setTimeout(() => {
-            if (!isCameraOpen) dispatch({ type: "FORCE_CAMERA_CLOSE" });
-        }, 10000);
-    };
-
-    async function isThisDoorOpen(door) {
-        return office[door];
+  useEffect(() => {
+    if (energy <= 0) {
+      isBlackout = true;
+      Ambience.pause();
     }
+  }, [energy]);
 
-    return (
+  useEffect(() => {
+    let newTime = 5000;
+    if (office.leftDoor) newTime -= 1100;
+    if (office.rightDoor) newTime -= 1100;
+    if (office.leftLight) newTime -= 500;
+    if (office.rightLight) newTime -= 500;
+    if (isCameraOpen) newTime -= 1100;
+
+    dispatch({ type: "CHANGE_TIME", content: newTime });
+    officeProps = {
+      leftDoor: office.leftDoor,
+      rightDoor: office.rightDoor,
+    };
+  }, [
+    office.leftDoor,
+    office.rightDoor,
+    office.leftLight,
+    office.rightLight,
+    isCameraOpen,
+  ]);
+
+  const handleJumpscare = (character) => {
+    if (isBlackout || gameOver) return;
+    dispatch({
+      type: "CHANGE_ANIMATRONIC",
+      animatronic: character,
+      animatronicState: {
+        door: null,
+        camera: null,
+        jumpscare: true,
+      },
+    });
+
+    dispatch({ type: "CHANGE_JUMPSCARE", animatronic: character });
+    if (character === "Foxy" || character === "Freddy")
+      dispatch({ type: "FORCE_CAMERA_CLOSE" });
+    setTimeout(() => {
+      if (!isCameraOpen) dispatch({ type: "FORCE_CAMERA_CLOSE" });
+    }, 10000);
+  };
+
+  async function isThisDoorOpen(door) {
+    const isDoorOpen = await officeProps[door];
+    return isDoorOpen;
+  }
+
+  return (
+    <>
+      <Animatronic
+        handleJumpscare={handleJumpscare}
+        gameOver={gameOver}
+        isThisDoorOpen={isThisDoorOpen}
+        blackout={energy <= 0}
+      />
+
+      {!gameOver ? (
         <>
-            <Animatronic
-                handleJumpscare={handleJumpscare}
-                gameOver={gameOver}
-                isThisDoorOpen={isThisDoorOpen}
-                blackout={energy <= 0}
-            />
-
-            {!gameOver ? (
-                <>
-                    {energy <= 0 ? null : <Hud />}
-                    <Camera handleJumpscare={handleJumpscare} />
-                    {isCameraOpen ? null : (
-                        <Office endGame={endGame} blackout={energy <= 0} />
-                    )}
-                </>
-            ) : null}
+          {energy <= 0 ? null : <Hud />}
+          <Camera handleJumpscare={handleJumpscare} />
+          {isCameraOpen ? null : (
+            <Office endGame={endGame} blackout={energy <= 0} />
+          )}
         </>
-    );
+      ) : null}
+    </>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        animatronics: state.animatronicsReducer,
-        time: state.configReducer.time,
-        hour: state.configReducer.hour,
-        energy: state.configReducer.energy,
-        office: state.officeReducer,
-        camera: state.cameraReducer.camera,
-        isCameraOpen: state.cameraReducer.isCameraOpen,
-    };
+  return {
+    animatronics: state.animatronicsReducer,
+    time: state.configReducer.time,
+    hour: state.configReducer.hour,
+    energy: state.configReducer.energy,
+    office: state.officeReducer,
+    camera: state.cameraReducer.camera,
+    isCameraOpen: state.cameraReducer.isCameraOpen,
+  };
 };
 
 export default connect(mapStateToProps)(Game);
--- src/components/Animatronic.js
+++ src/components/Animatronic.js
@@ -24,197 +24,191 @@ let isBlackout = false;
 let isGameOver = false;
 
 function Animatronic({
-    animatronics,
-    config,
-    handleJumpscare,
-    isThisDoorOpen,
-    dispatch,
+  animatronics,
+  config,
+  handleJumpscare,
+  isThisDoorOpen,
+  dispatch,
 }) {
-    const { hour, gameOver, blackout } = config;
+  const { hour, gameOver, blackout } = config;
 
-    useEffect(() => {
-        willMove("Bonnie", BonnieRange, BonnieIterator, BonnieTime);
-        willMove("Chica", ChicaRange, ChicaIterator, ChicaTime);
-        willMove("Foxy", FoxyRange, FoxyIterator, FoxyTime, true);
-        willMove("Freddy", FreddyRange, FreddyIterator, FreddyTime, true);
+  useEffect(() => {
+    willMove("Bonnie", BonnieRange, BonnieIterator, BonnieTime);
+    willMove("Chica", ChicaRange, ChicaIterator, ChicaTime);
+    willMove("Foxy", FoxyRange, FoxyIterator, FoxyTime, true);
+    willMove("Freddy", FreddyRange, FreddyIterator, FreddyTime, true);
 
-        return () => {
-            FreddyIterator = Functions.Freddy();
-            BonnieIterator = Functions.Bonnie();
-            ChicaIterator = Functions.Chica();
-            FoxyIterator = Functions.Foxy();
+    return () => {
+      FreddyIterator = Functions.Freddy();
+      BonnieIterator = Functions.Bonnie();
+      ChicaIterator = Functions.Chica();
+      FoxyIterator = Functions.Foxy();
 
-            FreddyIterator.next();
+      FreddyIterator.next();
 
-            FreddyTime = 10000;
-            BonnieTime = 3000;
-            ChicaTime = 6300;
-            FoxyTime = 13000;
-            FreddyRange = 1;
-            BonnieRange = 1;
-            ChicaRange = 2;
-            FoxyRange = 1;
+      FreddyTime = 10000;
+      BonnieTime = 3000;
+      ChicaTime = 6300;
+      FoxyTime = 13000;
+      FreddyRange = 1;
+      BonnieRange = 1;
+      ChicaRange = 2;
+      FoxyRange = 1;
 
-            isBlackout = false;
-            isGameOver = false;
-        };
-    }, []);
-
-    useEffect(() => {
-        if (hour === 2) {
-            FreddyTime = 9500;
-            BonnieTime = 2700;
-            ChicaTime = 5800;
-            FoxyTime = 10000;
-
-            BonnieRange += 1;
-            ChicaRange += 1;
-        } else if (hour === 4) {
-            BonnieRange += 1;
-            ChicaRange += 1;
-            FreddyRange += 1;
-            FoxyRange += 1;
-        }
-    }, [hour]);
-
-    useEffect(() => {
-        if (gameOver) isGameOver = gameOver;
-    }, [gameOver]);
-
-    const changeAnimatronic = (func) => {
-        dispatch({ type: "CHANGE_ANIMATRONICS_MOVING", content: true });
-
-        func();
-
-        setTimeout(() => {
-            dispatch({
-                type: "CHANGE_ANIMATRONICS_MOVING",
-                content: false,
-            });
-        }, 1500);
+      isBlackout = false;
+      isGameOver = false;
     };
+  }, []);
 
-    const animatronicFailed = (character) => {
-        changeAnimatronic(() => {
-            dispatch({
-                type: "CHANGE_ANIMATRONIC",
-                animatronic: character,
-                animatronicState: {
-                    door: false,
-                    camera:
-                        character === "Freddy"
-                            ? "Stage"
-                            : character === "Foxy"
-                            ? ""
-                            : "Dinning Area",
-                    jumpscare: false,
-                },
-            });
+  useEffect(() => {
+    if (hour === 2) {
+      FreddyTime = 9500;
+      BonnieTime = 2700;
+      ChicaTime = 5800;
+      FoxyTime = 10000;
 
-            if (character === "Bonnie") {
-                BonnieIterator = Functions.Bonnie();
-                willMove("Bonnie", BonnieRange, BonnieIterator, BonnieTime);
-            } else if (character === "Chica") {
-                ChicaIterator = Functions.Chica();
-                willMove("Chica", ChicaRange, ChicaIterator, ChicaTime);
-            } else if (character === "Foxy") {
-                FoxyIterator = Functions.Foxy();
-                Media.Sounds.FoxyPunch.play();
-                willMove("Foxy", FoxyRange, FoxyIterator, FoxyTime, true);
-            } else if (character === "Freddy") {
-                FreddyIterator = Functions.Freddy();
-                FreddyIterator.next();
-                willMove(
-                    "Freddy",
-                    FreddyRange,
-                    FreddyIterator,
-                    FreddyTime,
-                    true
-                );
-            }
-        });
-    };
-
-    const freddyLaugh = () => {
-        if (isBlackout) return;
-        let FreddyNumber = Math.floor(Math.random() * 2);
-        if (FreddyNumber == 0) {
-            Media.Sounds.FreddyLaugh1.play();
-        } else {
-            Media.Sounds.FreddyLaugh2.play();
-        }
-    };
-    useEffect(() => {
-        if (blackout) isBlackout = true;
-    }, [blackout]);
-
-    const willMove = (character, range, iterator, animaTime) => {
-        const thisInterval = setInterval(() => {
-            let luckyNumber = Math.floor(Math.random() * 5);
-
-            let condition =
-                luckyNumber < range && !animatronics[character].door;
-
-            let newPlace;
-            if (condition) {
-                changeAnimatronic(() => {
-                    newPlace = iterator.next().value;
-
-                    const newState = {
-                        door: newPlace === "Door" || newPlace === "_3",
-                        jumpscare: false,
-                        camera: newPlace,
-                    };
-                    dispatch({
-                        type: "CHANGE_ANIMATRONIC",
-                        animatronic: character,
-                        animatronicState: newState,
-                    });
-                });
-
-                if (character === "Freddy") freddyLaugh();
-            }
-
-            if (isBlackout || isGameOver) clearInterval(thisInterval);
-
-            if (newPlace === "Door" || newPlace === "_3") {
-                if (!isBlackout) checkDoors(character);
-                clearInterval(thisInterval);
-            }
-        }, animaTime);
-    };
-
-    async function checkDoors(character) {
-        const door =
-            character === "Bonnie" || character === "Foxy"
-                ? "leftDoor"
-                : "rightDoor";
-
-        setTimeout(() => {
-            if (!isThisDoorOpen(door)) {
-                setTimeout(() => {
-                    if (!isThisDoorOpen(door)) {
-                        setTimeout(() => {
-                            if (!isThisDoorOpen(door)) {
-                                handleJumpscare(character);
-                            } else animatronicFailed(character);
-                        }, 3000);
-                    } else animatronicFailed(character);
-                }, 5000);
-            } else animatronicFailed(character);
-        }, 10000);
+      BonnieRange += 1;
+      ChicaRange += 1;
+    } else if (hour === 4) {
+      BonnieRange += 1;
+      ChicaRange += 1;
+      FreddyRange += 1;
+      FoxyRange += 1;
     }
+  }, [hour]);
 
-    return <></>;
+  useEffect(() => {
+    if (gameOver) isGameOver = gameOver;
+  }, [gameOver]);
+
+  const changeAnimatronic = (func) => {
+    dispatch({ type: "CHANGE_ANIMATRONICS_MOVING", content: true });
+
+    func();
+
+    setTimeout(() => {
+      dispatch({
+        type: "CHANGE_ANIMATRONICS_MOVING",
+        content: false,
+      });
+    }, 1500);
+  };
+
+  const animatronicFailed = (character) => {
+    changeAnimatronic(() => {
+      dispatch({
+        type: "CHANGE_ANIMATRONIC",
+        animatronic: character,
+        animatronicState: {
+          door: false,
+          camera:
+            character === "Freddy"
+              ? "Stage"
+              : character === "Foxy"
+              ? ""
+              : "Dinning Area",
+          jumpscare: false,
+        },
+      });
+
+      if (character === "Bonnie") {
+        BonnieIterator = Functions.Bonnie();
+        willMove("Bonnie", BonnieRange, BonnieIterator, BonnieTime);
+      } else if (character === "Chica") {
+        ChicaIterator = Functions.Chica();
+        willMove("Chica", ChicaRange, ChicaIterator, ChicaTime);
+      } else if (character === "Foxy") {
+        FoxyIterator = Functions.Foxy();
+        Media.Sounds.FoxyPunch.play();
+        willMove("Foxy", FoxyRange, FoxyIterator, FoxyTime, true);
+      } else if (character === "Freddy") {
+        FreddyIterator = Functions.Freddy();
+        FreddyIterator.next();
+        willMove("Freddy", FreddyRange, FreddyIterator, FreddyTime, true);
+      }
+    });
+  };
+
+  const freddyLaugh = () => {
+    if (isBlackout) return;
+    let FreddyNumber = Math.floor(Math.random() * 2);
+    if (FreddyNumber == 0) {
+      Media.Sounds.FreddyLaugh1.play();
+    } else {
+      Media.Sounds.FreddyLaugh2.play();
+    }
+  };
+  useEffect(() => {
+    if (blackout) isBlackout = true;
+  }, [blackout]);
+
+  const willMove = (character, range, iterator, animaTime) => {
+    const thisInterval = setInterval(() => {
+      let luckyNumber = Math.floor(Math.random() * 5);
+
+      let condition = luckyNumber < range && !animatronics[character].door;
+
+      let newPlace;
+      if (condition) {
+        changeAnimatronic(() => {
+          newPlace = iterator.next().value;
+
+          const newState = {
+            door: newPlace === "Door" || newPlace === "_3",
+            jumpscare: false,
+            camera: newPlace,
+          };
+          dispatch({
+            type: "CHANGE_ANIMATRONIC",
+            animatronic: character,
+            animatronicState: newState,
+          });
+        });
+
+        if (character === "Freddy") freddyLaugh();
+      }
+
+      if (isBlackout || isGameOver) clearInterval(thisInterval);
+
+      if (newPlace === "Door" || newPlace === "_3") {
+        if (!isBlackout) checkDoors(character);
+        clearInterval(thisInterval);
+      }
+    }, animaTime);
+  };
+
+  async function checkDoors(character) {
+    const door =
+      character === "Bonnie" || character === "Foxy" ? "leftDoor" : "rightDoor";
+
+    setTimeout(async () => {
+      const isDoorOpen = await isThisDoorOpen(door);
+      if (!isDoorOpen) {
+        setTimeout(async () => {
+          const isDoorOpen = await isThisDoorOpen(door);
+          if (!isDoorOpen) {
+            setTimeout(async () => {
+              const isDoorOpen = await isThisDoorOpen(door);
+              if (!isDoorOpen) {
+                handleJumpscare(character);
+              } else animatronicFailed(character);
+            }, 3000);
+          } else animatronicFailed(character);
+        }, 5000);
+      } else animatronicFailed(character);
+    }, 10000);
+  }
+
+  return <></>;
 }
 
 const mapStateToProps = (state) => {
-    return {
-        leftDoor: state.officeReducer.leftDoor,
-        rightDoor: state.officeReducer.rightDoor,
-        animatronics: state.animatronicsReducer,
-        config: state.configReducer,
-    };
+  return {
+    leftDoor: state.officeReducer.leftDoor,
+    rightDoor: state.officeReducer.rightDoor,
+    animatronics: state.animatronicsReducer,
+    config: state.configReducer,
+  };
 };
 
 export default connect(mapStateToProps)(Animatronic);
